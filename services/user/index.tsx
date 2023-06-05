fe8bb7e1caaf84e81b65c49d8dae7c5591780a51
Gabriela <gpivetta99@gmail.com>
2023-06-05 18:40:40 -0300

    integra usuario com api além do firebase

--- services/user/index.tsx
+++ services/user/index.tsx
@@ -1,5 +1,22 @@
 import api from "../../config/api";
 
+export const getUser = async (firebase_uid: string): Promise<Result<User>> => {
+  try {
+    const res = await api.get<User>(`/users/user/uid/${firebase_uid}`);
+
+    return { type: "success", value: res.data } as unknown as Result<User>;
+  } catch (error) {
+    if (error instanceof Error)
+      return { type: "error", error, value: undefined };
+
+    return {
+      type: "error",
+      error: new Error("Erro desconhecido"),
+      value: undefined,
+    };
+  }
+};
+
 export const getAllUsers = async (): Promise<Result<User[]>> => {
   try {
     const res = await api.get<User>(`/users/`);
